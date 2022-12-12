b355932ac4dc2696fa2d7f550078dc4cbb491e94
Rishab Sah <you@example.com>
2022-12-12 17:04:04 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    title: "Recipe Finder",
+    subtitle: "HTML,CSS,JS",
+    description:
+      "We can find the recipe of the dishes through this app.The youtube links of the dishes have been made available dynamically.We just have to type the name of any ingredient.",
+    image: "./fr.png",
+    link: "https://foodrecipe-13.netlify.app/",
+  },
+  {
+    title: "Snake Game",
+    subtitle: "HTML CSS JS",
+    description:
+      "This a a simple snake game which works with all four arrow keys.The game gets over when the snake hits any boundary.CSS Grid has been used to build it.",
+    image: "./snake.png",
+    link: "https://snakegame13.netlify.app/",
+  },
+  {
+    title: "Restaurant Cocktail Menu",
+    subtitle: "React",
+    description:
+      "This is the cocktail menu of a restarant along with the detais of each cocktail.",
+    image: "./cock.png",
+    link: "https://cocktails-menu13.netlify.app/",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "This is a task management app to help you stay organised and manage your day-to-day. You can use it to make shopping lists or task lists and take notes.",
+    image: "./todo.png",
+    link: "https://grocerylist13.netlify.app/",
+  },
+  {
+    title: "Cart Summary",
+    subtitle: "React useReducer Hook",
+    description:
+      "This is summary of the cart of an E-Commerce website.The cart total updates and displays total sum after reducing or increasing items.",
+    image: "./cart.png",
+    link: "https://xcheckout-cart.netlify.app/",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node",
+  "MongoDB",
+  "Bootstrap",
+  "Material UI",
+];
